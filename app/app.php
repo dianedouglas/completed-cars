c0bf10cedaef27001be1be389ed87cc25420b5a9
Gibberish314 <a.moosen@gmail.com>
2016-09-06 12:35:13 -0400

    Add a route for my car form html file. It returns the exact same HTML to display my form, except that the form's action attribute is set to navigate me to my /cars route where I am displaying all of the cars the user has to choose from.

--- app/app.php
+++ app/app.php
@@ -21,6 +21,31 @@
           "<img src='". $my_picture ."'>";
     });
 
+    $app->get("/car_form", function(){
+        return "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <title>Find a Car</title>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Find a Car!</h1>
+                <form action='/cars'>
+                    <div class='form-group'>
+                        <label for='price'>Enter Maximum Price:</label>
+                        <input id='price' name='price' class='form-control' type='number'>
+                    </div>
+                    <button type='submit' class='btn-success'>Submit</button>
+                </form>
+            </div>
+        </body>
+        </html>
+
+        ";
+    });
+
     $app->get("/cars", function() {
         // Create car objects just as we did in the old version of Car.php
         // But this time use the constructor, and add images.
