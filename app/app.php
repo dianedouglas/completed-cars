01828da19220c591171a11b3a71b93694e4302d5
Gibberish314 <a.moosen@gmail.com>
2016-09-06 11:02:56 -0400

    Load the car class into app.php, instantiate a car object for testing, and call its methods to display info about it.

--- app/app.php
+++ app/app.php
@@ -1,13 +1,22 @@
 <?php
     date_default_timezone_set('America/New_York');
     // date_default_timezone_set('America/Los_Angeles');
-    
+
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Car.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Hi there!";
+        $my_new_car = new Car("Porsche", 25, 1000);
+        $my_model = $my_new_car->getModel();
+        $my_price = $my_new_car->getPrice();
+        $my_miles = $my_new_car->getMiles();
+
+        return "I somehow have this amazingly cheap awesome new car!" . 
+          " It is a " . $my_model . 
+          " with only " . $my_miles . " miles on it, " . 
+          "and I only paid: $" . $my_price . "!";
     });
 
     return $app;
