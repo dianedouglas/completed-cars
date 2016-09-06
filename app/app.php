4ca5a3a10b8913a1b7544c7d622e018a7d258d35
Gibberish314 <a.moosen@gmail.com>
2016-09-06 11:07:38 -0400

    Add a picture URL to the particular car object that I am instantiating in app.php and then display it by calling the getPicture method, storing the returned value in a variable, and then using it in an image tag.

--- app/app.php
+++ app/app.php
@@ -8,15 +8,17 @@
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        $my_new_car = new Car("Porsche", 25, 1000);
+        $my_new_car = new Car("Porsche", 25, 1000, "http://gtspirit.com/wp-content/uploads/2014/08/porsche-boxster-gts-e1442738533400-696x385.jpg");
         $my_model = $my_new_car->getModel();
         $my_price = $my_new_car->getPrice();
         $my_miles = $my_new_car->getMiles();
+        $my_picture = $my_new_car->getPicture();
 
         return "I somehow have this amazingly cheap awesome new car!" . 
           " It is a " . $my_model . 
           " with only " . $my_miles . " miles on it, " . 
-          "and I only paid: $" . $my_price . "!";
+          "and I only paid: $" . $my_price . "!" . 
+          "<img src='". $my_picture ."'>";
     });
 
     return $app;
