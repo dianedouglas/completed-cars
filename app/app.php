a43ffde1a280b52769dd54a82c318b1067b83c04
Gibberish314 <a.moosen@gmail.com>
2016-09-06 10:39:03 -0400

    Fix timezone error better.

--- app/app.php
+++ app/app.php
@@ -1,5 +1,7 @@
 <?php
-    date_default_timezone_set('UTC');
+    date_default_timezone_set('America/New_York');
+    // date_default_timezone_set('America/Los_Angeles');
+    
     require_once __DIR__."/../vendor/autoload.php";
 
     $app = new Silex\Application();
